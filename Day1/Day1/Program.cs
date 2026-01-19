7862513cde278d72d64588458f33c04d7202f803
Sơn Hoàng <sondh209@gmail.com>
2026-01-19 20:26:04 +0700

    Update bai tap

--- Day1/Day1/Program.cs
+++ Day1/Day1/Program.cs
@@ -23,4 +23,48 @@ Console.WriteLine($"So tuan la: {totalMinutes / 60}, so ngay con lai la: {totalM
 
 
 //Bai tap 4
+Console.WriteLine("Nhap so tien: ");
+int amount1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Nhap so % giam gia: ");
+float vat = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"So tien giam gia la: {amount1 * discount / 100}");
+Console.WriteLine($"So tien phai tra la: {amount1 + (amount1 * discount / 100)}");
 
+//Bai tap 5
+Console.WriteLine("Nhap so tien USD: ");
+float usd = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Nhap ti gia: ");
+float rate = float.Parse(Console.ReadLine()!);
+Console.WriteLine($"So tien VND la: {usd * rate}");
+
+//Bai tap 6
+Console.WriteLine("Nhap so tien trong tai khoan: ");
+float balance = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Nhap so tien muon rut: ");
+float withdraw = float.Parse(Console.ReadLine()!);
+Console.WriteLine($"Rut tien thanh cong. So du con lai: {balance - withdraw}");
+
+//Bai tap 7
+Console.WriteLine("Nhap vao quang duong (km): ");
+float distance = float.Parse(Console.ReadLine()!);
+Console.WriteLine("Nhap vao thoi gian (gio): ");
+float time = float.Parse(Console.ReadLine()!);
+Console.WriteLine($"Van toc trung binh la: {distance / time} km/h");
+
+//Bai tap 8
+Console.WriteLine("Nhap vao mot so: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Nhap vao 1 tong so: ");
+int sum = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Ti le % cua so tren tong so la: {number/sum * 100}%");
+
+//Bai tap 9
+Console.WriteLine("Nhap vao van toc km/h: ");
+float speed = float.Parse(Console.ReadLine()!);
+Console.WriteLine($"Van toc m/s la: {speed * 0.27} m/s");
+
+//Bai tap 10
+Console.WriteLine("Nhap vao so phut da tap the duc: ");
+int exerciseMinutes = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"So calo da dot chay khi chay la: {exerciseMinutes * 12} calos ," +
+    $"So calo da dot chay khi boi la: {exerciseMinutes * 55} calos ");
